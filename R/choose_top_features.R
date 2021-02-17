abd740b68f05e68cdc9af3945c56180fc8f96815
Michael D. Kessler <mkessl11@jhu.edu>
2021-02-17 11:30:05 -0500

    fix choose_top_features bug

--- R/choose_top_features.R
+++ R/choose_top_features.R
@@ -15,8 +15,27 @@ choose_top_features <- function(vec){
   #TODO - add more regarding how k is chosen and what makes it into top features
   
   # kmeans to separate groups of prioritized features
-  km <- stats::kmeans(vec,3)
-  top_features <- names(km$cluster)[km$cluster %in% as.numeric(names(sort(table(km$cluster))[1:2]))]
+  # you will do these with a trycatch block to handle cases where there are not enough distinct values to have three clusters
+  
+  km <- tryCatch({
+    stats::kmeans(vec,3)
+  }, warning = function(cond) {
+    message(paste0("kmeans warning k = : ",3))
+    message("Here's the original warning message:")
+    message(cond)
+    # Choose a return value in case of warning
+    return(NULL)
+  }, error = function(cond) {
+    # rerun kmeans with k = 2
+      stats::kmeans(vec,2)
+  }, finally=NULL)
+  
+  
+  if (length(km$centers) == 3){
+    top_features <- names(km$cluster)[km$cluster %in% as.numeric(names(sort(table(km$cluster))[1:2]))]
+  } else{
+    top_features <- names(km$cluster)[km$cluster %in% as.numeric(names(sort(table(km$cluster))[1]))]
+  }
   
   return(top_features)
 }
